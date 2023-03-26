567c0fb0d6969316df1e58bc7d91bc999a375c42
GitHub Action <action@github.com>
2023-03-26 22:45:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/isksha/xmlplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/isksha/xmlplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.isksha.xmlplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.isksha.xmlplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
